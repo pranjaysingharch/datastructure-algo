e9a26b755d2cd9e91d102219ba0fb447fe5fe179
Singh <SINGHP@HILTI.com>
2025-09-12 11:36:26 +0530

    optimized version

--- datastructureproblems-python/hashmap/longestconsecutivesequence.py
+++ datastructureproblems-python/hashmap/longestconsecutivesequence.py
@@ -14,7 +14,27 @@ def longest_chain_of_consecutive_numbers_brute_force(nums: List[int]) -> int:
             current_chain += 1
         longest_chain = max(longest_chain, current_chain)
     return longest_chain
+def longest_chain_of_consecutive_numbers(nums: List[int]) -> int:
+    if not nums:
+        return 0
+    num_set = set(nums)
+    longest_chain = 0
+    for num in num_set:
+        # If the current number is the smallest number in its chain, search for
+        # the length of its chain.
+        if num - 1 not in num_set:
+            current_num = num
+            current_chain = 1
+            # Continue to find the next consecutive numbers in the chain.
+            while current_num + 1 in num_set:
+                current_num += 1
+                current_chain += 1
+            longest_chain = max(longest_chain, current_chain)
+    return longest_chain
+
+
 
 if __name__ == "__main__":
    nums = [1, 6, 2, 5, 8, 7, 10, 3,4]
-   print(longest_chain_of_consecutive_numbers_brute_force(nums))
+   #print(longest_chain_of_consecutive_numbers_brute_force(nums))
+   print(longest_chain_of_consecutive_numbers(nums))
